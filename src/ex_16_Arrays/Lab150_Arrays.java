1e61d56ac01ae0eb48c74e4d0132836ed5757d5a
Priya <itw.priyag@gmail.com>
2025-06-25 23:46:49 +0530

    array

---
+++ src/ex_16_Arrays/Lab150_Arrays.java
@@ -0,0 +1,14 @@
+package ex_16_Arrays;
+
+public class Lab150_Arrays {
+    public static void main(String[] args){
+        int[] marks = {1,2,3,4,5};
+        int[] mark2 = new int[5];
+        String[] names = new String[3];
+        names[0] = "Priya";
+        names[1] = "Gurajapu";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(marks[2]);
+    }
+}
